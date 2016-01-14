98fe4536076a4a842d1d4b3a20eaeee45a92154a
Callum Jago <cjago@ufl.edu>
2016-01-14 15:06:10 -0500

    test

--- listingController.js
+++ listingController.js
@@ -2,13 +2,20 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.detail = false;
+    $scope.ind = undefined;
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+     this.detail = !this.detail;
+     $scope.ind = index;
+    };
   }
 ]);
